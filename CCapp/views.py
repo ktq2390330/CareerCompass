927588a9b1a3b9752b917dc6e27ff1498e17c9fb
Frisk9673 <ktq2390301@stu.o-hara.ac.jp>
2024-11-01 11:41:04 +0900

    LoginView(暫定)

--- CCapp/views.py
+++ CCapp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .models import User
+from .forms import LoginForm
 
 # TemplateViewをインポート
 from django.views.generic.base import TemplateView
@@ -8,3 +12,24 @@ class IndexView(TemplateView):
 
 class LoginView(TemplateView):
     template_name = 'login.html'
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm()
+        return self.render_to_response({"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            mail = form.cleaned_data["mail"]
+            password = form.cleaned_data["password"]
+
+            try:
+                user = User.objects.get(mail=mail)
+                if user.password == password:  #簡易的なパスワードチェックを実行
+                    login(request, user)
+                    return redirect("home")  #ログイン後のリダイレクト先を指定する
+                else:
+                    messages.error(request, "パスワードが正しくありません。")
+            except User.DoesNotExist:
+                messages.error(request, "ユーザーが存在しません。")
+        return self.render_to_response({"form": form})
