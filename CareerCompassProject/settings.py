d242d9f09e9d8c2a33caf7dc9c046e95011cc904
at39ohara <ktq2390327@stu.o-hara.ac.jp>
2024-11-13 12:42:56 +0900

    filter_baseとfilter_areaのcssがちゃんと反映されるようになった。

--- CareerCompassProject/settings.py
+++ CareerCompassProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'CCapp.apps.CcappConfig',
-    "django_cleanup",
+    # "django_cleanup",
 ]
 
 MIDDLEWARE = [
