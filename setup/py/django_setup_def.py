ae03a49a937a2407cb6c9870c6a9e7e87d1aef22
ktq2390330 <ktq2390330@stu.o-hara.ac.jp>
2024-11-24 01:35:04 +0900

    remove cache

--- setup/py/django_setup_def.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import django
-#Djangoプロジェクトの設定
-def djangoImportSetup():
-	project_root=os.path.abspath(os.path.join(os.path.dirname(__file__),'..','..'))
-	sys.path.append(project_root)
-	os.environ.setdefault('DJANGO_SETTINGS_MODULE','CareerCompassProject.settings')
-	django.setup()
