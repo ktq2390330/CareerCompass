ff94a7fa85938962191a05db54b1ce1bc3213381
ktq2390330 <ktq2390330@stu.o-hara.ac.jp>
2025-01-14 15:11:46 +0900

    rm: cached .

--- CCapp/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='get_item')
-def get_item(dictionary, key):
-    """辞書からキーに対応する値を取得するカスタムフィルター"""
-    return dictionary.get(key)
