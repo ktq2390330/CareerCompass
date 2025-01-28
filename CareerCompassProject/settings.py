200d6861231fb2057f89ee51162ea2fb547e1a0e
ktq2390330 <ktq2390330@stu.o-hara.ac.jp>
2025-01-28 15:20:46 +0900

    fix: settings.py CSRF_ORIGINS

--- CareerCompassProject/settings.py
+++ CareerCompassProject/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS','').split(',')
 
 
 # Application definition
